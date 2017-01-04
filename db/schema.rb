94094c3c6f5868074a9824c2732d2b619e4cf8e5
medxiaorudan <729323503@qq.com>
2017-01-04 20:11:43 +0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# encoding: UTF-8
+ # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20161123101856) do
     t.integer  "teacher_id"
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
-    t.boolean  "open"
+    t.boolean  "open"                     ,default:false
     t.boolean  "course_open",   default: false
   end
 
