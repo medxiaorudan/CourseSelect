a0241f18393ea73c8215e6d792fd8863b5bdda34
medxiaorudan <729323503@qq.com>
2017-01-04 20:09:47 +0800

    Update 20170102023706_add_open_attribute_to_courses.rb

--- db/migrate/20170102023706_add_open_attribute_to_courses.rb
+++ db/migrate/20170102023706_add_open_attribute_to_courses.rb
@@ -1,6 +1,6 @@
 class AddOpenAttributeToCourses < ActiveRecord::Migration
   def change
     add_column :courses, :open, :Boolean
-    change_column_default :courses, :open, from: true, to: false  
+    change_column_default :courses, :open, from: false, to: true  
   end
 end
